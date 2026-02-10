74e4841eda8cb090c2597090409974690648e5a5
K2222810 <axelbaynes@gmail.com>
2026-02-10 02:48:34 +0000

    Testing buttom update and delete in application page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Dashboard from './components/Dashboard/Dashboard';
 import AppList from './components/AppList/AppList.jsx';
 import * as appService from './services/appService.js';
 import * as followUpService from './services/appFollowUp.js';
+import * as checkInService from './services/checkInService.js';
 import AppDetails from './components/AppDetails/AppDetails.jsx';
 import AppForm from "./components/AppForm/AppForm";
 import FollowUpsPage from './components/FollowUpsPage/FollowUpsPage.jsx';
@@ -26,6 +27,27 @@ const handleAddApp = async (appFormData) => {
   const newApp = await appService.create(appFormData);
   setApplications([newApp, ...applications]);
   navigate("/applications");
+};
+
+const handleUpdateApp = async (appId, appFormData) => {
+  try {
+    const updatedApp = await appService.update(appId, appFormData);
+    setApplications(applications.map((app) => (appId === app._id ? updatedApp : app)));
+    navigate(`/applications/${appId}`);
+    return updatedApp;
+  } catch (error) {
+    console.error('Error updating application:', error);
+    return null;
+  }
+};
+
+const handleDeleteApp = async (appId) => {
+  const deletedApp = await appService.deleteApp(appId);
+  if (deletedApp) {
+    setApplications(applications.filter((app) => app._id !== deletedApp._id));
+    navigate('/applications');
+  }
+  return deletedApp;
 };
 
 const handleAddFollowUp = async (followUpFormData) => {
@@ -50,6 +72,22 @@ const handleUpdateFollowUp = async (appId, followUpId, followUpFormData) => {
     return null;
   }
 };
+
+const handleUpdateCheckIn = async (appId, checkInId, checkInFormData) => {
+  try {
+    const updatedCheckIn = await checkInService.update(appId, checkInId, checkInFormData);
+    return updatedCheckIn;
+  } catch (error) {
+    console.error('Error updating check-in:', error);
+    return null;
+  }
+};
+
+const handleDeleteCheckIn = async (appId, checkInId) => {
+  const deletedCheckIn = await checkInService.deleteCheckIn(appId, checkInId);
+  return deletedCheckIn;
+};
+
   useEffect(() => {
   const fetchAllApplications = async () => {
     const applicationData = await appService.index();
@@ -68,8 +106,9 @@ const handleUpdateFollowUp = async (appId, followUpId, followUpFormData) => {
           {user ? (
           <>
           <Route path='/applications' element={<AppList applications={applications} />} />
-          <Route path='/applications/:appId' element={<AppDetails/>} />
+          <Route path='/applications/:appId' element={<AppDetails handleDeleteApp={handleDeleteApp} handleUpdateCheckIn={handleUpdateCheckIn} handleDeleteCheckIn={handleDeleteCheckIn} />} />
           <Route path="/applications/new" element={<AppForm handleAddApp={handleAddApp} />} />
+          <Route path="/applications/:appId/edit" element={<AppForm handleUpdateApp={handleUpdateApp} />} />
           <Route path="/follow-ups" element={<FollowUpsPage handleAddFollowUp={handleAddFollowUp} handleDeleteFollowUp={handleDeleteFollowUp} handleUpdateFollowUp={handleUpdateFollowUp} />} />
           </>
         ) : (
